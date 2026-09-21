2dbca4ec3b59b66e00e6717e9a54263404336da4
Joe Rutland <243651362+rutlandjoe-crypto@users.noreply.github.com>
2026-09-21 14:32:58 -0500

    Add Lone Star to GSR Network top rail

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,7 @@ const GSR_NETWORK = [
   ["Politics", "https://globalpoliticsreport.com"],
   ["Entertainment", "https://globalentertainmentreport.com"],
   ["Betting", "https://globalbettingreport.com"],
+  ["Lone Star", "https://www.gsrlonestarreport.com/"],
 ];
 
 const DEFAULT_URL = "https://variety.com";
